558bf642192c59a218f6e67d8ed7db067a213943
Steapa <santoslucas347@gmail.com>
2024-04-20 22:47:33 -0300

    Menu image

--- menu.py
+++ menu.py
@@ -18,14 +18,23 @@ buttons = {
     3:{"text":"QUIT","size":20,"obj":None}
 }
 
+imgs = {
+    0:{"name":"snaketitle.png","pos":(0,0),"obj":None}
+}
+
 class TextButton():
     
     def __init__(self,size) -> None:
         self.font =  pygame.freetype.Font(os.path.join(glbs.assetsDir,"fonts/DejaVuSerif-Bold.ttf"),size)
         self.selected = False
 
-class img():
-    pass
+class Image(py.sprite.Sprite):
+
+    def __init__(self,img):
+        py.sprite.Sprite.__init__(self)
+        self.image = py.image.load(os.path.join(glbs.assetsDir+"/imgs",img))
+        self.image = self.image.convert_alpha()
+        self.rect = self.image.get_rect()
 
 class Menu():
 
@@ -42,6 +51,9 @@ class Menu():
 
         for btn in buttons:
             buttons[btn]["obj"] = TextButton(buttons[btn]["size"])
+
+        for img in imgs:
+            imgs[img]["obj"] = Image(imgs[img]["name"])
  
 
     #menu logic
@@ -62,6 +74,10 @@ class Menu():
             _obj.font.render_to(screen,(_scr_pos.x,_scr_pos.y),_txt,(255,255,255))
             
             _spacing+=Menu._btn_spacing
+
+        for img in imgs:
+            py.sprite.Group(imgs[img]["obj"]).draw(screen)
+            
 
         #_btn1_size = self.btn1.font.get_rect(Menu.btn1Text).size
 
